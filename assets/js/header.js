37d3e06ef63f787bae77a8f835e85111f2eba833
Pedro Sousa <pedrovictorsousa69@gmail.comd>
2023-09-03 03:49:49 -0300

    atualizando o navbar

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,7 +2,9 @@ function abrirMenu () {
     let menuMobile = document.querySelector('.menu-mobile');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('.mobile-menu-icon').src = "./assets/img/close-icon.svg";
     } else {
         menuMobile.classList.add('open');
+        document.querySelector('.mobile-menu-icon').src = "./assets/img/menu-icon.svg";
     }
 }
