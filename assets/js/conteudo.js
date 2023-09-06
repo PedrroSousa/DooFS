1d177c6c7eb764e3f3a365400ea5d5cd84d9b862
Pedro Sousa <pedrovictorsousa69@gmail.comd>
2023-09-06 18:14:17 -0300

    Adicionando pagina de conteudo

--- assets/js/conteudo.js
+++ assets/js/conteudo.js
@@ -14,3 +14,43 @@ function sair () {
     window.location.href = 'index.html';
 }
 document.querySelector('#sair-btn').onclick = sair;
+
+const apikey = '1c9ff41c'
+
+const formBusca = document.querySelector('form');
+
+formBusca.onsubmit = (evento) => {
+    evento.preventDefault();
+    
+    const buscar = evento.target.busca.value;
+
+    let msgError = document.querySelector('#msgError')
+
+    if (buscar == "") {
+        msgError.innerHTML = 'Campo de pesquisa vazio'
+        return;
+    } else {
+        msgError.innerHTML = 'Resultados'
+    }
+
+    fetch(`https://www.omdbapi.com/?apikey=${apikey}&s=${buscar}`)
+        .then(result => result.json())
+        .then(json => carregaLista(json));
+}
+
+const carregaLista = (json) => {
+    const lista = document.querySelector(".lista-item");
+    lista.innerHTML = "";
+
+    json.Search.forEach(element => {
+        console.log(element)
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        item.innerHTML = `<img src="${element.Poster}" /><h2>${element.Title}</h2>`
+
+        lista.appendChild(item);
+
+        
+    });
+}
